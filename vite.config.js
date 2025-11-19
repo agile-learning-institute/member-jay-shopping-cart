96b1301d68adf4633293f3bbc20deac82696342f
Jay C <jaycenusa@gmail.com>
2025-11-19 11:20:06 -0500

    feat: first commit using github action

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'shopping-cart',
+  base: '/shopping-cart',
   plugins: [
     react(),
     tailwindcss(),
